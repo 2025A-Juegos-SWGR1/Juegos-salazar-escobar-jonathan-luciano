74fd46e60f0e579aeae3395f000ffa66af373cfe
jluSalazar <jonathan282salazar@gmail.com>
2025-06-21 19:54:54 -0500

    Added: Material de clase

---
+++ 01 - carrito/carrito/Assets/Driver.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] private float velocidadGiro = 85f;
+    [SerializeField] private float velocidadMovimiento = 6f;
+    [SerializeField] private float velocidadGiroRapido = 105f;
+    [SerializeField] private float velocidadMovimientoRapido = 10f;
+    [SerializeField] private float velocidadGiroLento = 60f;
+    [SerializeField] private float velocidadMovimientoLento = 4f;
+    // Update is called once per frame
+    void Update()
+    {
+        float valorGiro = Input.GetAxis("Horizontal") * velocidadGiro * Time.deltaTime;
+        float valorMovimiento = Input.GetAxis("Vertical") * velocidadMovimiento * Time.deltaTime;
+        transform.Rotate(0,0,-valorGiro);
+        transform.Translate(0,valorMovimiento,0);
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "velocidad")
+        {
+            velocidadGiro = velocidadGiroRapido;
+            velocidadMovimiento = velocidadMovimientoRapido;
+            Destroy(other.gameObject);
+        }
+        if (other.tag == "lento")
+        {
+            velocidadGiro = velocidadGiroLento;
+            velocidadMovimiento = velocidadMovimientoLento;
+            
+        }
+    }
+}
